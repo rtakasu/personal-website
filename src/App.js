90bfdcf7ffd68964fd0a8c7b2a6302b60f813e4c
Rafael Takasu <rafatakasu@gmail.com>
2018-03-13 19:52:12 -0700

    Skeleton for Tabs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Tab, Tabs, TabList, TabPanel} from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 
 class App extends Component {
     render() {
@@ -11,7 +13,24 @@ class App extends Component {
                     <h1 className="App-title">Rafael Takasu</h1>
                 </header>
 
+
+                  <Tabs>
+                    <TabList>
+                      <Tab>Title 1</Tab>
+                      <Tab>Title 2</Tab>
+                    </TabList>
+
+                    <TabPanel>
+                      <h2>Any content 1</h2>
+                    </TabPanel>
+                    <TabPanel>
+                      <h2>Any content 2</h2>
+                    </TabPanel>
+                  </Tabs>
+
+
                 <p className="App-intro">
+
                     To get started, edit <code>src/App.js</code> and save to reload.
                 </p>
             </div>
